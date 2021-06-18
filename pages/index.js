fa79e55d797b9935a26e9d3c102c91cbe8b4e884
benmangold <benmangold@gmail.com>
2021-06-18 13:08:59 -0400

    init blog starter

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,51 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
 
-import Card from "../components/Card";
-
-import Footer from "../components/Footer";
-
-import Meta from "../components/Meta";
-
-export default function Home() {
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
   return (
-    <div className={styles.container}>
-      <Meta />
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=.8, shrink-to-fit=no"
-        />
-        <main className={styles.main}>
-          <h1 className={styles.title}>benmangold.com</h1>
-
-          <p className={styles.description}>
-            A website for <code className={styles.code}>writing</code>
-            {" , "}
-            <code className={styles.code}>engineering</code>
-            {" and "}
-            <code className={styles.code}>audio</code>
-          </p>
-          <div className={styles.grid}>
-            <Card
-              href="/pageOne"
-              header="Page One &rarr;"
-              text="Just a test page. Not a lot going on here."
+    <>
+      <Layout>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
             />
-            <Card
-              href="/pageTwo"
-              header="Page Two"
-              text="check check one two hey hey one two hey hey."
-            />
-          </div>
-        </main>
-      </Head>
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
+}
 
-      <Footer />
-    </div>
-  );
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
 }
