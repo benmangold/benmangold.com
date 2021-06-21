9c3d84e9ec938ef231ef43ff67c7e6ae17ffc7ff
Ben Mangold <benmangold@gmail.com>
2021-06-20 23:55:51 -0400

    taglist on posts

--- components/post-header.js
+++ components/post-header.js
@@ -3,7 +3,9 @@ import DateFormatter from "../components/date-formatter";
 import CoverImage from "../components/cover-image";
 import PostTitle from "../components/post-title";
 
-export default function PostHeader({ title, coverImage, date, author }) {
+import TagList from "../components/tag-list"
+
+export default function PostHeader({ title, coverImage, date, author, tags }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
@@ -13,6 +15,7 @@ export default function PostHeader({ title, coverImage, date, author }) {
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} height={620} width={1240} />
       </div>
+      <TagList tags={tags}/>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
