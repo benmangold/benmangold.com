b845ccce7919447d4c960e85247c52bc8c41e8bf
Ben Mangold <benmangold@gmail.com>
2021-06-23 00:11:18 -0400

    clean up span

--- components/blog.js
+++ components/blog.js
@@ -5,7 +5,7 @@ export default function Blog({ allPosts }) {
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
   return (
-    <span>
+    <>
       {" "}
       {heroPost && (
         <HeroPost
@@ -19,7 +19,6 @@ export default function Blog({ allPosts }) {
         />
       )}
       {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-    </span>
+    </>
   );
 }
-
