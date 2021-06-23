60553005ca3f6b9e7dd3aa7e29be02f2826c2af0
Ben Mangold <benmangold@gmail.com>
2021-06-22 23:38:15 -0400

    clean up tag list

--- components/tag-list.js
+++ components/tag-list.js
@@ -1,6 +1,6 @@
 export default function TagList({ tags }) {
   return (
-    <div class="flex space-x-4">
+    <div>
       {tags.split(" ").map((tag) => {
         return <Tag tag={tag} />;
       })}
@@ -10,13 +10,11 @@ export default function TagList({ tags }) {
 
 function Tag({ tag }) {
   return (
-    <div>
       <span
         class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2 align-middle"
         style={{ outline: "none", border: "0" }}
       >
         {tag}
       </span>
-    </div>
   );
 }
