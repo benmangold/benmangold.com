9de8d3cd335d7e5e5e266bdad659c021a271c91b
benmangold <benmangold@gmail.com>
2021-06-26 12:04:14 -0400

    tag sorting

--- components/tag-list.js
+++ components/tag-list.js
@@ -16,14 +16,20 @@ export default function TagList({ tags, selectedTags, setSelectedTags }) {
 
 function Tag({ tag, selectedTags, setSelectedTags }) {
   return (
-    <span
-      className="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2 align-middle"
-      style={{ outline: "none", border: "0" }}
-      onClick={() => {
-        setSelectedTags([...selectedTags, tag]);
-      }}
-    >
-      {tag}
-    </span>
+    <>
+      <span
+        className="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2 align-middle"
+        style={{ outline: "none", border: "0" }}
+        onClick={() => {
+          if (!selectedTags.includes(tag)) {
+            setSelectedTags([...selectedTags, tag]);
+          }
+        }}
+      >
+        {tag + ' '}
+        {selectedTags.includes(tag) ? <button onClick={event => { event.stopPropagation(); setSelectedTags([]) }}>x</button> : null}
+      </span>
+    </>
+
   );
 }
