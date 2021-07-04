d1c08e53dc9d8bda35d08f3fabc76eed6cb2cb0c
Ben Mangold <benmangold@gmail.com>
2021-07-04 15:14:55 -0400

    landing page

--- components/intro.js
+++ components/intro.js
@@ -1,11 +1,31 @@
-export default function Intro() {
+import Link from "next/link";
+
+
+export default function Intro({description = ""}) {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         benmangold.com
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A personal blog
+        {description}
+        <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About Me</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+          </ul>
       </h4>
     </section>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Container from "../components/container";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
 import Head from "next/head";
-
 import Blog from "../components/blog";
+import { getAllPosts } from "../lib/api";
+
 
 export default function Index({ allPosts }) {
   return (
@@ -14,8 +14,9 @@ export default function Index({ allPosts }) {
           <title>benmangold.com</title>
         </Head>
         <Container>
-          <Intro />
+          <Intro description="A personal website." />
           <Blog allPosts={allPosts} />
+
         </Container>
       </Layout>
     </>
